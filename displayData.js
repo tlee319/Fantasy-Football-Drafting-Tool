7dcde98c172ea067fa640b27c3f10da840f4a5f1
Thomas Lee <ThomasLee@Thomass-MacBook-Pro.local>
2017-08-05 21:09:19 -0400

    push

--- displayData.js
+++ displayData.js
@@ -5,10 +5,30 @@ function paint() {
             width = +svg.attr("width") - margin.left - margin.right,
             height = +svg.attr("height") - margin.top - margin.bottom;
 
+        var svg2 = d3.select("svg"),
+            margin = {top: 20, right: 20, bottom: 30, left: 100},
+            width = +svg.attr("width") - margin.left - margin.right,
+            height = +svg.attr("height") - margin.top - margin.bottom;
+
         var x = d3.scaleLinear().range([0, width]);
         var y = d3.scaleBand().range([height, 0]);
         var g = svg.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x2 = d3.scaleLinear().range([0, width]);
+        var y2 = d3.scaleBand().range([height, 0]);
+        var g2 = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain([0.0, 1.0]);
+        y.domain(playerInfoList.map(function (d) {
+            return d["name"]
+        })).padding(0.1);
+
+        playerInfoList.sort(function (a, b) {
+            //console.log("a: " + a["performancePercentile"] + ", b: " + b["performancePercentile"] + ", diff: " + (a["performancePercentile"] - b["performancePercentile"]));
+            return a["performancePercentile"] - b["performancePercentile"];
+        })
 
         x.domain([0.0, 1.0]);
         y.domain(playerInfoList.map(function (d) {
@@ -28,6 +48,15 @@ function paint() {
 
         console.log("this shit");
         console.log(playerInfoList);
+
+        /*
+        playerInfoList.sort(function (a, b) {
+            console.log(a["performancePercentile"]);
+            return a["performancePercentile"] - b["performancePercentile"];
+        }); */
+
+        console.log(playerInfoList);
+        console.log(playerNameList);
 
         g.selectAll(".bar")
             .data(playerInfoList)
